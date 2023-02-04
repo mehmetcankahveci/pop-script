91be0e1cf09334b67846b48e58829f01ca571e61
Mehmet Can Kahveci <96494651+mehmetcankahveci@users.noreply.github.com>
2023-02-04 22:06:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ sudo apt install gnome-tweaks
 
 sudo apt install curl
 
+sudo apt install yaru-theme-gnome-shell yaru-theme-gtk yaru-theme-icon yaru-theme-sound
+
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
